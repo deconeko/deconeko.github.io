f01b00cde6f4be9e03bb3eb3d89ae994509c5de7
deconeko <migumame.45328da@gmail.com>
2022-08-16 15:17:16 +0900

    title変更

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Index = () => {
   return (
     <>
-      <div>Welcome to Next.js!</div>
+    <Head>
+      <title>Mgr030(deconeko) Portfolio</title>
+    </Head>
       <Link href="/linkSample">
         <a>link Sample Page</a>
       </Link>
