855ae0232b1b5820bdc60e05aeb457a0ffe2e58a
deconeko <migumame.45328da@gmail.com>
2022-08-17 17:40:13 +0900

    Revert ".next&node_modulesをgit管理対象から除外"
    This reverts commit 56d7269c9c3980498ee4ccd580ec9fef262d11fb.

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/linkSample":["static\u002Fchunks\u002Fpages\u002FlinkSample.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002FlinkSample"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
